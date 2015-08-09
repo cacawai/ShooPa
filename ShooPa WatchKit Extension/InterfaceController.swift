d0a10ae6dddc8dcbf96597132d51cd3a09a9639c
cacawai <xuxinyuan@iMac-5K.local>
2015-08-09 21:58:26 +0800

    Updated for xcode7 beta5

--- ShooPa WatchKit Extension/InterfaceController.swift
+++ ShooPa WatchKit Extension/InterfaceController.swift
@@ -8,13 +8,19 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
-
+    let motionMgr:CMMotionManager = CMMotionManager()
+    let motionQueue:NSOperationQueue = NSOperationQueue.mainQueue()
+    let assetURL = NSBundle.mainBundle().URLForResource("fart", withExtension: "wav")
+    
+    @IBOutlet var testLabel: WKInterfaceLabel!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        coreMotionAcceleroMeterTest()
         // Configure interface objects here.
     }
 
@@ -28,4 +34,42 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func onClickPlayBtn() {
+        self.playSoundByMediaPlayerController(assetURL!)
+    }
+    
+    func playSoundByAudioFilePlayer(assetURL: NSURL) {
+        let asset = WKAudioFileAsset(URL: assetURL)
+        let playerItem = WKAudioFilePlayerItem(asset: asset)
+        let audioFilePlayer = WKAudioFilePlayer(playerItem: playerItem)
+            
+        if (audioFilePlayer.status == WKAudioFilePlayerStatus.ReadyToPlay) {
+            audioFilePlayer.play()
+        }
+    }
+    
+    func playSoundByMediaPlayerController(url: NSURL) {
+        
+        self.presentMediaPlayerControllerWithURL(url, options: [WKMediaPlayerControllerOptionsAutoplayKey: true]) { (didPlayToEnd: Bool, endTime: NSTimeInterval, err: NSError?) -> Void in
+            if (!didPlayToEnd) {
+                print("The player did not play all the way to the end. The player only played until time - \(endTime)\n")
+            }
+            
+            if err != nil{
+                print("There was an error with playback:" + (err?.description)! + "\n")
+            }
+        }
+    }
+    
+    func coreMotionAcceleroMeterTest()
+    {
+        motionMgr.accelerometerUpdateInterval = 0.1
+        motionMgr.startAccelerometerUpdatesToQueue(motionQueue) { (accel: CMAccelerometerData?, err: NSError?) -> Void in
+            let acceleration:CMAcceleration = accel!.acceleration
+//            self.testLabel.setText("X:\(acceleration.x) Y:\(acceleration.y) Z:\(acceleration.z)")
+            if abs(acceleration.x) > 1 || abs(acceleration.y) > 1 || abs(acceleration.z) > 1 {
+                self.playSoundByMediaPlayerController(self.assetURL!)
+            }
+        }
+    }
 }
